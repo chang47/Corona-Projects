fe0bb6826150381fde97d21128ff22be7aa30c87
chang47 <coralbue@gmail.com>
2014-09-15 23:24:03 -0700

    playing wih images

--- main.lua
+++ main.lua
@@ -5,4 +5,13 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
-print( 'bye' );
+-- textObject = display.newText( "Hello World", 100, 50, "Times New Roman", 36 );
+-- textObject:setTextColor( 255, 255, 255, 100 );
+local background = display.newImage('wall.jpg', true);
+background.x = display.contentWidth/2;
+background.y = display.contentHeight;
+-- local image01 = display.newImage('small.jpg', 110, 30);
+-- local image02 = display.newImage('small.jpg');
+-- image02.x = 160; image02.y = 200;
+-- image03 = display.newImage( "small.jpg" );
+-- image03.x = 160; image03.y = 320;
