94cad9091171650dedaab6da86e1f2081f9c14f7
chang47 <coralbue@gmail.com>
2014-09-17 15:14:39 -0700

    started making game

--- Breakout/main.lua
+++ Breakout/main.lua
@@ -5,3 +5,25 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+display.setStatusBar( HiddenStatusBar )
+
+local physics = require "physics"
+
+physics.start()
+physics.setGravity(0, 0)
+
+system.setAccelerometerInterval( 100 )
+
+local menuScreenGroup
+local mmScreen
+local playBtn
+
+local background
+local paddle
+local brick
+local ball
+
+local scoreText
+local scoreNum
+local levelText
+local levelNum
