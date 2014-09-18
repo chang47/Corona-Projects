901091ea931e7618345fa623bcb41aa543b6f6f6
chang47 <coralbue@gmail.com>
2014-09-18 07:33:14 -0700

    added play screen

--- Breakout/main.lua
+++ Breakout/main.lua
@@ -27,3 +27,36 @@ local scoreText
 local scoreNum
 local levelText
 local levelNum
+
+print("hi")
+
+function loadGame( event )
+	if event.target.name == "playbutton" then
+		transition.to(menuScreenGroup, time{time = 0, alpha=0, onComplete=addGameScreen})
+		playBtn.removeEventListener('tap', loadGame)
+	end
+end
+
+function mainMenu()
+	menuScreenGroup = display.newGroup()
+	mmScreen = display.newImage('img/mmScreen.png', 0, 0, true)
+	print(_W)
+	mmScreen.x = _W
+	mmScreen.y = _H
+
+	playBtn = display.newImage('img/playBtn.png')
+	--playBtn:setReferencePoint(display.CenterReferencePoint)
+	playBtn.x = _W; playBtn.y = _H + 50
+	playBtn.name = "playbutton"
+
+	menuScreenGroup:insert(mmScreen)
+
+	menuScreenGroup:insert(playBtn)
+	playBtn:addEventListener("tap", loadGame)
+end
+
+function main()
+	mainMenu()	
+end
+
+main()
