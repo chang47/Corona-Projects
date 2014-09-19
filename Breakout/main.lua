6474f3a80f1d7c955c38bddab75290e9a748fc58
chang47 <coralbue@gmail.com>
2014-09-19 14:45:34 -0700

    filled first screen

--- Breakout/main.lua
+++ Breakout/main.lua
@@ -56,7 +56,6 @@ function loadGame( event )
 	if event.target.name == "playbutton" then
 		transition.to(menuScreenGroup, {time = 0, alpha=0, onComplete=addGameScreen})
 		playBtn:removeEventListener('tap', loadGame)
-		img = display.newCircle( _W, _H, 10 )
 	end
 end
 
@@ -78,8 +77,104 @@ function mainMenu()
 	playBtn:addEventListener("tap", loadGame)
 end
 
-function main()
-	mainMenu()	
+function addGameScreen() 
+	background = display.newImage("img/bg.png", 0, 0, true)
+	background.x = _W
+	background.y = _H
+
+	paddle = display.newImage("img/paddle.png")
+	paddle.x = 240; paddle.y = 300
+	paddle.name = "paddle"
+
+	ball = display.newImage("img/ball.png")
+	ball.x = 240; ball.y = 290
+	ball.name = "ball"
+
+	scoreText = display.newText("Score:", 100, 10, "Arial", 14)
+	scoreText:setTextColor(255,255,255,255)
+
+	scoreNum = display.newText("0", 140, 10, "Arial", 14)
+	scoreNum:setTextColor(255, 255,255,255)
+
+	levelText = display.newText("Level:", 420, 10, "Arial", 14)
+	levelText:setTextColor(255,255,255,255)
+
+	levelNum = display.newText("1", 460, 10, "Arial", 14)
+	levelNum:setTextColor(255,255,255,255)
+
+	gameLevel1()
+end
+
+function gameLevel1(  )
+	currentLevel = 1
+	bricks:toFront()
+
+	local numOfRows = 4
+	local numOfColumns = 4
+	local brickPlacement = {x = _W - (brickWidth * numOfColumns) / 2 + 20, y= 50}
+	
+	for row = 0, numOfRows - 1 do
+		for column=0, numOfColumns - 1 do
+			local brick = display.newImage("img/brick.png")
+			brick.name = "brick"
+			brick.x = brickPlacement.x + (column * brickWidth)
+			brick.y = brickPlacement.y + (row * brickHeight)	
+			physics.addBody(brick, "static", {density = 1, friction = 0, bounce = 0})
+			bricks.insert(bricks, brick) -- ???
+		end
+	end
+end
+
+function gameLevel2(  )
+	currentLevel = 2
+	bricks:toFront()
+
+	local numOfRows = 5
+	local numOfColumns = 8
+	local brickPlacement = {x = _W - (brickWidth * numOfColumns) / 2 + 20, y= 50}
+	
+	for row=0,numOfRows - 1 do
+		for col=0,numOfColumns - 1 do
+			local brick = display.newImage("img/brick.png")
+			brick.name = "brick"
+			brick.x = brickPlacement.x + (column * brickWidth)
+			brick.y = brickPlacement.y + (row * brickHeight)	
+			physics.addBody(brick, "static", {density = 1, friction = 0, bounce = 0})
+			bricks.insert(bricks, brick) -- ???
+		end
+	end
+end
+
+function alertScreen( title, message )
+	alertBox = display.newImage("alertBox.png")
+	alertBox.x = 240; alertBox.y = 160
+	transition.from(alertBox, {time = 500, xScale = 0.5, yScale = 0.5,
+		transition = easing.outExpo})
+
+	conditionDisplay = display.newText(title, 0, 0, "Arial", 38)
+	conditionDisplay:setTextColor(255,255,255,255)
+	conditionDisplay.xScale = 0.5
+	conditionDisplay.yScale = 0.5
+	--
+	conditionDisplay.x = display.contentCenterX
+	conditionDisplay.y = display.contentHeightY - 15
+
+	messageText = display.newText(message, 0, 0, "Arial", 24)
+	messageText:setTextColor(255,255,255,255)
+	messageText.xScale = 0.5
+	messageText.yScale = 0.5
+	--
+	messageText.x = display.contentCenterX
+	messageText.y = display.contentHeightY
+
+	alertDisplayGroup = display.newGroup()
+	alertDisplayGroup:insert(alertBox)
+	alertDisplayGroup:insert(conditionDisplay)
+	alertDisplayGroup:insert(messageText)
+end
+
+function main(  )
+	mainMenu()
 end
 
 main()
